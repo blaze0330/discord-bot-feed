68ae94806896a9d827c8badb76269dd78b1ccc43
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-11-28 22:44:43 +0530

    updated logic to find latest article

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import requests
 import pytz
 import pandas as pd
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.parser import parse
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from xml.etree.ElementTree import fromstring, ElementTree
@@ -15,8 +15,8 @@ print("Est Time Now", est_timenow)
 
 
 url = "https://www.nasa.gov/rss/dyn/breaking_news.rss"
-discord_webhook_url = ""
-
+# discord_webhook_url = "https://discord.com/api/webhooks/940522536557498368/un_14JP-n7T6haQzIhsDB9Dfj_XjWB_2wYFK6oD-opeC-ZfRhgH29_LSbWnNGnOzGi5B"
+discord_webhook_url = "https://discord.com/api/webhooks/940522536557498368/un_14JP-n7T6haQzIhsDB9Dfj_XjWB_2wYFK6oD-opeC-ZfRhgH29_LSbWnNGnOzGi5B"
 
 def create_get_request(url):
     """ create a get request to the url and return the response """
@@ -24,26 +24,26 @@ def create_get_request(url):
     return response
 
 
-def send_message_to_discord(title, description, link, pubDate):
+def send_message_to_discord(title, description, link, pubDate, image_url):
     """ Send message to discord """
     webhook = DiscordWebhook(url=discord_webhook_url, username="Nasa News Bot")
     embed = DiscordEmbed(title=title, description=description, color=242424)
     embed.add_embed_field(name="Published at", value=str(pubDate))
     embed.set_url(link)
     embed.set_author(name='Developer(Codeperfectplus)', url='https://github.com/codeperfectplus')
-    embed.set_footer(text='Nasa News Bot v1.0',)    
+    embed.set_footer(text='Nasa News Bot v1.0')
+    # add embed image     
     webhook.add_embed(embed)
     webhook.execute()
 
 
 def sleep_decorator(function):
     def wrapper(*args, **kwargs):
-        sleep(60)
         function(*args, **kwargs)
+        sleep(60*60*12)  # 12 hours
     return wrapper
 
-
-@sleep_decorator
+# @sleep_decorator
 def send_message(response):
     """ 
     main function to iterate over the response and send message to discord
@@ -53,16 +53,17 @@ def send_message(response):
 
     for item in root.iter('item'):
         title = item.find('title').text
-        image = item.find('enclosure').attrib['url']
+        image_url = item.find('enclosure').attrib['url']
         description = item.find('description').text
         link = item.find('link').text
         pubDate = parse(item.find('pubDate').text)
         
-        print(pubDate, est_timenow)
         
-        if pubDate > est_timenow:
-            print("New Nasa News")
-            send_message_to_discord(title, description, link, pubDate)
+        # check if there is any new news published in last 12 hours
+        est_timezone  = est_timezone - timedelta(hours=12) 
+        if pubDate > est_timezone:
+            send_message_to_discord(title, description, link, pubDate, image_url)
+            print("Message sent to discord")
                 
 
 def main():
